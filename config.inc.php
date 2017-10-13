b33a484781e8fe37178a5378f66d948074422e7b
stefanoimperiale <30474939+stefanoimperiale@users.noreply.github.com>
2017-10-13 15:49:00 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['host'] = 'lomux';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
